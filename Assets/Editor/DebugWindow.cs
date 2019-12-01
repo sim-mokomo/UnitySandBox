52ccdd63b8b6814ba2367c504c78746d446a55d9
sim-mokomo <15795655+sim-mokomo@users.noreply.github.com>
2019-12-01 12:26:02 +0900

    impl: DebugWindowの基礎を作成

---
+++ Assets/Editor/DebugWindow.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class DebugWindow : EditorWindow
+{
+    [MenuItem ("MokomoGames/DebugWindow")]
+    public static void Open ()
+    {
+        EditorWindow.CreateWindow<DebugWindow> ();
+    }
+}
