c61c229829df4e93c3cc7e96ee44f8a0f8b5649c
sim-mokomo <15795655+sim-mokomo@users.noreply.github.com>
2019-12-01 15:54:31 +0900

    impl: デバッグフラグ定義用クラス作成

---
+++ Assets/Script/DebugFlag.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace MokomoGames
+{
+    namespace Debug
+    {
+        public class DebugFlag
+        {
+            public bool Active { get; set; }
+            public string Description { get; private set; }
+
+            public DebugFlag (string description)
+            {
+                this.Description = description;
+            }
+        }
+
+        public enum DebugFlagType
+        {
+            DisplayTransformPlayerData,
+            DisplayFixPlayerData,
+        }
+    }
+}
