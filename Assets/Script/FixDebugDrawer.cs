4469f427789a856a1e466220bc74fea47071b894
sim-mokomo <15795655+sim-mokomo@users.noreply.github.com>
2019-12-01 15:15:57 +0900

    impl: 固定デバッグ描画表示

---
+++ Assets/Script/FixDebugDrawer.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using MokomoGames.Debug;
+using UnityEngine;
+
+public class FixDebugDrawer : IDebugDrawer
+{
+    Vector2 DrawScreenPos { get; set; }
+
+    public FixDebugDrawer (Vector2 DrawScreenPos)
+    {
+        this.DrawScreenPos = DrawScreenPos;
+    }
+
+    public void Draw (Vector2 screenPos, string debugMessage)
+    {
+        GUI.Label (
+            new Rect (
+                screenPos.x,
+                screenPos.y,
+                300f,
+                30f
+            ),
+            debugMessage
+        );
+    }
+
+    public Vector2 GetOriginDrawScreenPos ()
+    {
+        return DrawScreenPos;
+    }
+}
