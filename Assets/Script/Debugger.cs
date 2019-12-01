a92d72f9168fb6a1b5f84762496995272c84123b
sim-mokomo <15795655+sim-mokomo@users.noreply.github.com>
2019-12-01 14:24:27 +0900

    impl: デバッグ表示

---
+++ Assets/Script/Debugger.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace MokomoGames
+{
+    namespace Debug
+    {
+        public class Debugger
+        {
+            public static void DisplayDebugModel (IDebugModel model, IDebugDrawer drawer)
+            {
+                var debugMessages = model.GetDebugMessages ();
+                var originScreenPos = drawer.GetOriginDrawScreenPos ();
+
+                for (int i = 0; i < debugMessages.Length; i++)
+                {
+                    var labelScreenPos = CalcScreenPosWithSpan (originScreenPos, drawOrder : i);
+                    var message = debugMessages[i];
+                    drawer.Draw (labelScreenPos, message);
+                }
+            }
+
+            public static class Config
+            {
+                public static bool DisplayPlayerData;
+            }
+
+            static Vector2 CalcScreenPosWithSpan (Vector2 screenPos, int drawOrder)
+            {
+                screenPos.y += 15f * drawOrder;
+                screenPos.y = Screen.height - screenPos.y;
+                return screenPos;
+            }
+        }
+    }
+}
