8ce4c4ccb9ec11a5ce6720757275990b3b7a4c1b
sim-mokomo <15795655+sim-mokomo@users.noreply.github.com>
2019-12-01 14:26:12 +0900

    impl: プレイヤー詳細情報表示、Editorから設定できるように

--- Assets/Editor/DebugWindow.cs
+++ Assets/Editor/DebugWindow.cs
@@ -1,18 +1,33 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using MokomoGames.Debug;
 using UnityEditor;
 using UnityEngine;
 
 namespace MokomoGames
 {
-    namespace Debug
+    namespace Editor
     {
-        public class DebugWindow : EditorWindow
+        namespace Debug
         {
-            [MenuItem ("MokomoGames/DebugWindow")]
-            public static void Open ()
+            public class DebugWindow : EditorWindow
             {
-                EditorWindow.CreateWindow<DebugWindow> ();
+                static bool displayDebugModel;
+
+                [MenuItem ("MokomoGames/DebugWindow")]
+                public static void Open ()
+                {
+                    EditorWindow.CreateWindow<DebugWindow> ();
+                }
+
+                public void OnGUI ()
+                {
+                    using (new EditorGUILayout.HorizontalScope (GUI.skin.box))
+                    {
+                        EditorGUILayout.LabelField ("プレイヤー詳細情報表示");
+                        Debugger.Config.DisplayPlayerData = EditorGUILayout.Toggle (Debugger.Config.DisplayPlayerData);
+                    }
+                }
             }
         }
     }
