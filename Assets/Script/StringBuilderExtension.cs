df74758e81e181c176d681508b4c9204985eb1c5
sim-mokomo <15795655+sim-mokomo@users.noreply.github.com>
2019-12-30 20:44:45 +0900

    impl: タブ付き AppendLine実装

---
+++ Assets/Script/StringBuilderExtension.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+
+namespace MokomoGames
+{
+    public static class StringBuilderExtention
+    {
+        public static void AppendLineWithTab(this StringBuilder self, bool enableTab, string message)
+        {
+            if(enableTab)
+                self.Append("\t");
+            self.AppendLine(message);
+        }
+    }
+}
