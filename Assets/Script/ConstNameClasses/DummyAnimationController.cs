18905f061574953e6c7eeea80187e76327621848
sim-mokomo <15795655+sim-mokomo@users.noreply.github.com>
2019-12-30 21:32:31 +0900

    impl: 出力テスト用フォルダ作成＆テスト

---
+++ Assets/Script/ConstNameClasses/DummyAnimationController.cs
@@ -0,0 +1,19 @@
+public static class DummyAnimationController
+{
+	public static class Layer1
+	{
+		public static readonly string Test1 = "Test1";
+
+		public static readonly string Test2 = "Test2";
+
+	}
+
+	public static class Layer2
+	{
+		public static readonly string Test1 = "Test1";
+
+		public static readonly string Test2 = "Test2";
+
+	}
+
+}
---
+++ Assets/Script/ConstNameClasses/DummyAnimationController_1.cs
@@ -0,0 +1,19 @@
+public static class DummyAnimationController_1
+{
+	public static class Layer3
+	{
+		public static readonly string Test1 = "Test1";
+
+		public static readonly string Test2 = "Test2";
+
+	}
+
+	public static class Layer4
+	{
+		public static readonly string Test1 = "Test1";
+
+		public static readonly string Test2 = "Test2";
+
+	}
+
+}
