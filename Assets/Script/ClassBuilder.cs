70c1e68457a01d06798d326758075bae7bda753c
sim-mokomo <15795655+sim-mokomo@users.noreply.github.com>
2019-12-30 20:45:23 +0900

    impl: クラスと変数用のBuilderの作成

---
+++ Assets/Script/ClassBuilder.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+
+namespace MokomoGames
+{
+    public class ClassBuilder
+    {
+        private string _className;
+        private List<VariableBuilder> _variables = new List<VariableBuilder>();
+        private List<ClassBuilder> _classBuilders = new List<ClassBuilder>();
+
+        public ClassBuilder(string className)
+        {
+            _className = className;
+        }
+        
+        public void AddClasses(ClassBuilder classBuilder)
+        {
+            _classBuilders.Add(classBuilder);
+        }
+
+        public void AddVariables(IReadOnlyList<VariableBuilder> variables)
+        {
+            foreach (var variable in variables)
+            {
+                _variables.Add(variable);
+            }
+        }
+
+        public string Format(bool enableTab=false)
+        {
+            var builder = new StringBuilder();
+            builder.AppendLineWithTab(enableTab,$"public static class {_className}");
+            builder.AppendLineWithTab(enableTab,"{");
+            
+            foreach (var classBuilder in _classBuilders)
+            {
+                builder.AppendLine(classBuilder.Format(enableTab: true));
+            }
+            
+            foreach (var variable in _variables)
+            {
+                builder.AppendLine(variable.Format(enableTab: true));
+            }
+            
+            builder.AppendLineWithTab(enableTab,"}");
+            return builder.ToString();
+        }
+    }
+}
---
+++ Assets/Script/VariableBuilder.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+
+namespace MokomoGames
+{
+    public class VariableBuilder
+    {
+        public string Name { get; private set; }
+        public string DefaultValue { get; private set; }
+
+        public VariableBuilder(string name, string defaultValue)
+        {
+            this.Name = name;
+            this.DefaultValue = defaultValue;
+        }
+
+        public string Format(bool enableTab=false)
+        {
+            var builder = new StringBuilder();
+            builder.AppendLineWithTab(enableTab: enableTab,$"\tpublic static readonly string {Name} = \"{DefaultValue}\";");
+            return builder.ToString();
+        }
+    }
+}
