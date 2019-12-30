cf3d094ff183a68ce49571b66867f54470fbbae3
sim-mokomo <15795655+sim-mokomo@users.noreply.github.com>
2019-12-30 15:21:59 +0900

    impl: ステート名を定数クラスとして書き出しできるように

---
+++ Assets/Editor/GenerateAnimatorStateList.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using UnityEngine;
+using UnityEditor;
+using UnityEditor.Animations;
+
+public class GenerateAnimatorStateList : EditorWindow
+{
+    private AnimatorController _targetAnimatorController;
+    private UnityEngine.Object _outputDistDirectory;
+
+    [MenuItem("MokomoGames/Animatorのステート名定数クラスを生成")]
+    public static void Open()
+    {
+        GetWindow<GenerateAnimatorStateList>();
+    }
+
+    public bool AllowOutput => _targetAnimatorController != null && _targetAnimatorController != null;
+
+    private string GetOutputFilePath(UnityEngine.Object outputDirectory,AnimatorController animatorController)
+    {
+        var basePath = AssetDatabase.GetAssetPath(_outputDistDirectory);
+        var fileName = Path.GetFileNameWithoutExtension(AssetDatabase.GetAssetPath(animatorController));
+        return $"{basePath}/{fileName}.cs";
+    }
+
+    private void OnGUI()
+    {
+        _targetAnimatorController = EditorGUILayout.ObjectField(
+            "ステート名を定数クラスに抽出するAnimator",
+            _targetAnimatorController,
+            typeof(AnimatorController),
+            allowSceneObjects: false) as AnimatorController;
+
+        using (new EditorGUILayout.VerticalScope(GUI.skin.box))
+        {
+            _outputDistDirectory = EditorGUILayout.ObjectField(
+                "定数クラス書き出し先フォルダ",
+                _outputDistDirectory,
+                typeof(UnityEngine.Object),
+                allowSceneObjects:false);
+
+            var outputDescription =
+                AllowOutput ? GetOutputFilePath(_outputDistDirectory, _targetAnimatorController) : "設定が不十分です";
+            EditorGUILayout.LabelField($"書き出し先→{outputDescription}");
+        }
+        
+        EditorGUILayout.LabelField("↓抽出されるステート、レイヤーごとに分割される。");
+        using(new EditorGUILayout.VerticalScope(GUI.skin.box))
+        {
+            if(_targetAnimatorController == null)
+                return;
+            foreach (var layer in _targetAnimatorController.layers)
+            {
+                EditorGUILayout.LabelField($"レイヤー名:{layer.name}",EditorStyles.boldLabel);
+                foreach (var state in layer.stateMachine.states)
+                {
+                    EditorGUILayout.LabelField(state.state.name);
+                }
+            }
+        }
+
+        if (GUILayout.Button("抽出開始"))
+        {
+            if(_targetAnimatorController == null)
+                return;
+            
+            StringBuilder builder = new StringBuilder();
+            builder.AppendLine($"public static class {_targetAnimatorController.name}");
+            builder.AppendLine("{");
+
+            foreach (var layer in _targetAnimatorController.layers)
+            {
+                builder.AppendLine($"public static class {layer.name}");
+                builder.AppendLine("{");
+                foreach (var state in layer.stateMachine.states)
+                {
+                    builder.AppendLine($"public static readonly string {state.state.name};");
+                }
+
+                builder.AppendLine("}");
+            }
+            
+            builder.AppendLine("}");
+
+            var path = GetOutputFilePath(_outputDistDirectory, _targetAnimatorController);
+            File.WriteAllText(path,builder.ToString());
+            AssetDatabase.Refresh();
+        }
+    }
+}
---
+++ Assets/Script/DummyAnimationController.cs
@@ -0,0 +1,14 @@
+public static class DummyAnimationController
+{
+    public static class Layer1
+    {
+        public static readonly string Test1;
+        public static readonly string Test2;
+    }
+    
+    public static class Layer2
+    {
+        public static readonly string Test1;
+        public static readonly string Test2;
+    }
+}
